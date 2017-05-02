e947c5de884fce7cf91dc5a4471b501c780d649d
Syahrul Ubaidilah <syahrul923a@gmail.com>
2017-05-02 14:07:38 +0700

    Move return view to controller

--- routes/web.php
+++ routes/web.php
@@ -15,38 +15,17 @@ Route::get('/', function () {
     return view('pages.dashboard');
 })->name('page.dashboard');
 
-Route::get('/list-guru', function () {
-    return view('pages.list-guru');
-})->name('page.list-guru');
+Route::get('/siswa','Pages\UserController@siswa')->name('page.list-siswa');
+Route::get('/guru','Pages\UserController@guru')->name('page.list-guru');
+Route::get('/staf','Pages\UserController@staf')->name('page.list-staf');
+Route::get('/users/create','Pages\UserController@create')->name('page.create-user');
+Route::get('/users/{id}/edit','Pages\UserController@edit')->name('page.edit-user');
 
-Route::get('/list-siswa', function () {
-    return view('pages.list-siswa');
-})->name('page.list-siswa');
-
-Route::get('/list-staf', function () {
-    return view('pages.list-staf');
-})->name('page.list-staf');
-
-Route::get('/list-agenda', function () {
-    return view('pages.list-activity');
-})->name('page.list-agenda');
+Route::get('/activity','Pages\ActivityController@index')->name('page.list-activity');
+Route::get('/activity/create','Pages\ActivityController@create')->name('page.create-activity');
+Route::get('/activity/{id}/edit','Pages\ActivityController@edit')->name('page.edit-activity');
 
 Route::get('/calendar', function () {
     return view('pages.calendar');
 })->name('page.calendar');
 
-Route::get('/user/create', function () {
-    return view('pages.create-user');
-})->name('page.create-user');
-
-Route::get('/agenda/create', function () {
-    return view('pages.create-activity');
-})->name('page.create-agenda');
-
-Route::get('/user/edit', function () {
-    return view('pages.update-user');
-})->name('page.edit-user');
-
-Route::get('/agenda/edit', function () {
-    return view('pages.update-activity');
-})->name('page.edit-agenda');
