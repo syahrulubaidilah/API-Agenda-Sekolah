f99b300e6c352928e2edfaba3aedb311440b9f9e
Edi Santoso <edicyber@gmail.com>
2017-05-08 08:38:46 +0700

    fix search users/staf

--- app/Domain/Repositories/UserRepository.php
+++ app/Domain/Repositories/UserRepository.php
@@ -8,115 +8,109 @@ use App\Domain\Contracts\Crudable;
 
 
 /**
- * Class UserRepository
- * @package App\Domain\Repositories
- */
+* Class UserRepository
+* @package App\Domain\Repositories
+*/
 class UserRepository extends AbstractRepository implements UserInterface, Crudable
 {
-
+    
     /**
-     * @var User
-     */
+    * @var User
+    */
     protected $model;
-
+    
     /**
-     * UserRepository constructor.
-     * @param User $user
-     */
+    * UserRepository constructor.
+    * @param User $user
+    */
     public function __construct(User $user)
     {
         $this->model = $user;
     }
-
+    
     /**
-     * @return \Illuminate\Database\Eloquent\Collection|static[]
-     */
+    * @return \Illuminate\Database\Eloquent\Collection|static[]
+    */
     public function getAll()
     {
         return $this->model->all();
     }
-
+    
     /**
-     * @param int $limit
-     * @param int $page
-     * @param array $column
-     * @param string $field
-     * @param string $search
-     * @return \Illuminate\Pagination\Paginator
-     */
+    * @param int $limit
+    * @param int $page
+    * @param array $column
+    * @param string $field
+    * @param string $search
+    * @return \Illuminate\Pagination\Paginator
+    */
     public function paginate($limit = 10, $page = 1, array $column = ['*'], $field, $search = '')
     {
         // query to aql
         return parent::paginate($limit, $page, $column, 'name', $search);
     }
-
+    
     /**
-     * @param array $data
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+    * @param array $data
+    * @return \Symfony\Component\HttpFoundation\Response
+    */
     public function create(array $data)
     {
         // execute sql insert
         return parent::create([
-            'name'    => e($data['name']),
-            'email'   => e($data['email']),
-            'password' => bcrypt($data['password']),
-            'level'   => '1',
-            'position'   => e($data['position'])
+        'name'    => e($data['name']),
+        'email'   => e($data['email']),
+        'password' => bcrypt($data['password']),
+        'level'   => '1',
+        'position'   => e($data['position'])
         ]);
-
+        
     }
     /**
-     * @param $id
-     * @param array $data
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+    * @param $id
+    * @param array $data
+    * @return \Symfony\Component\HttpFoundation\Response
+    */
     public function update($id, array $data)
     {
         return parent::update($id, [
-            'name'    => e($data['name']),
-            'email'   => e($data['email']),
-            // 'password' => e($data['password']),
-            'level'   => e($data['level']),
-            'position'   => e($data['position'])
+        'name'    => e($data['name']),
+        'email'   => e($data['email']),
+        // 'password' => e($data['password']),
+        'level'   => e($data['level']),
+        'position'   => e($data['position'])
         ]);
     }
-
+    
     /**
-     * @param $id
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
+    * @param $id
+    * @return \Symfony\Component\HttpFoundation\Response
+    */
     public function delete($id)
     {
         return parent::delete($id);
     }
-
-
+    
+    
     /**
-     * @param $id
-     * @param array $columns
-     * @return mixed
-     */
+    * @param $id
+    * @param array $columns
+    * @return mixed
+    */
     public function findById($id, array $columns = ['*'])
     {
         return parent::find($id, $columns);
     }
-
+    
     public function getList($limit = 10, $page = 1, array $column = ['*'], $field, $search = '')
     {
         // query to aql
         $user = $this->model
-            ->where('level' == '1')
-            ->where(function ($query) use ($search) {
-                $query->where('name', 'like', '%' . $search . '%');
-
-            })
-            ->select('level' == '1')
-            ->paginate($limit)
-            ->toArray();
-
-        return $user();
-        // return parent::paginateWhere($limit, $page, $column, 'level', $level);
+        ->where('name', 'like', '%' . $search . '%')
+        ->where('level','1')
+        ->paginate($limit);
+        
+        return $user;
     }
-
+    
 }
