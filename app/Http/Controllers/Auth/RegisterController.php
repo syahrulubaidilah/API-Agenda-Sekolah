c0ed24e4702f6ee1c7a839ba310a3d03c85be4d2
Syahrul Ubaidilah <syahrul923a@gmail.com>
2017-05-26 08:17:58 +0700

    fix register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'level' => '0',
+            'position' => 'Siswa'
         ]);
     }
 }
