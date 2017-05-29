d91806361ce19e5c9995018f30bd84408ea33c78
Syahrul Ubaidilah <syahrul923a@gmail.com>
2017-05-29 12:34:13 +0700

    routing landingpage

--- app/Http/Controllers/LandingpageController.php
+++ app/Http/Controllers/LandingpageController.php
@@ -13,7 +13,7 @@ class LandingpageController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('guest');
     }
 
     /**
--- resources/views/layouts/landingpage.blade.php
+++ resources/views/layouts/landingpage.blade.php
@@ -9,18 +9,9 @@
 
 		<link href="css/master.css" rel="stylesheet">
 
-		<!-- SWITCHER -->
-		<link rel="stylesheet" id="switcher-css" type="text/css" href="assets/switcher/css/switcher.css" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color1.css" title="color1" media="all" data-default-color="true" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color2.css" title="color2" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color3.css" title="color3" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color4.css" title="color4" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color5.css" title="color5" media="all" />
-		<link rel="alternate stylesheet" type="text/css" href="assets/switcher/css/color6.css" title="color6" media="all" />
-
 		<!--[if lt IE 9]>
-		<script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-		<script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<script src={{asset('//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv .js')}}></script>
+		<script src={{asset('//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min .js')}}></script>
 		<![endif]-->
 
 	</head>
@@ -30,28 +21,6 @@
 		<div id="page-preloader"><span class="spinner"></span></div>
 		<!-- Loader end -->
 		<!-- Start Switcher -->
-		<div class="switcher-wrapper">	
-			<div class="demo_changer">
-				<div class="demo-icon customBgColor"><i class="fa fa-cog fa-spin fa-2x"></i></div>
-				<div class="form_holder">
-					<div class="row">
-						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-							<div class="predefined_styles">
-								<div class="skin-theme-switcher">
-									<h4>Color</h4>
-									<a href="#" data-switchcolor="color1" class="styleswitch" style="background-color:#f76d2b;"> </a>
-									<a href="#" data-switchcolor="color2" class="styleswitch" style="background-color:#de483d;"> </a>
-									<a href="#" data-switchcolor="color3" class="styleswitch" style="background-color:#228dcb;"> </a>
-									<a href="#" data-switchcolor="color4" class="styleswitch" style="background-color:#00bff3;"> </a>
-									<a href="#" data-switchcolor="color5" class="styleswitch" style="background-color:#2dcc70;"> </a>
-									<a href="#" data-switchcolor="color6" class="styleswitch" style="background-color:#6054c2;"> </a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
 		<!-- End Switcher -->
 
 		<header class="b-topBar wow slideInDown" data-wow-delay="0.7s">
@@ -103,14 +72,14 @@
 							<div class="collapse navbar-collapse navbar-main-slide" id="nav">
 								<ul class="navbar-nav-menu">
 									<li>
-										<a href="home.html">Home </a>
+										<a href={{route('page.home')}}>Home </a>
 										
 									</li>
 									<li class="dropdown">
 										<a class="dropdown-toggle" data-toggle='dropdown' href="#">Grid <span class="fa fa-caret-down"></span></a>
 										<ul class="dropdown-menu h-nav">
-											<li><a href="listings.html">Listing</a></li>
-											<li><a href="listTable.html">Table</a></li>
+											<li><a href={{route('page.list')}}>Listing</a></li>
+											<li><a href={{route('page.table')}}>Table</a></li>
 										</ul>
 									</li>
 								</ul>
@@ -158,30 +127,30 @@
 			</div>
 		</footer><!--b-footer-->
 		<!--Main-->   
-		<script src="js/jquery-1.11.3.min.js"></script>
-		<script src="js/jquery-ui.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/modernizr.custom.js"></script>
+		<script src={{asset('js/jquery-1.11.3.min .js')}}></script>
+		<script src={{asset('js/jquery-ui.min .js')}}></script>
+		<script src={{asset('js/bootstrap.min .js')}}></script>
+		<script src={{asset('js/modernizr.custom .js')}}></script>
 
-		<script src="assets/rendro-easy-pie-chart/dist/jquery.easypiechart.min.js"></script>
-		<script src="js/waypoints.min.js"></script>
-		<script src="js/jquery.easypiechart.min.js"></script>
-		<script src="js/classie.js"></script>
+		<script src={{asset('assets/rendro-easy-pie-chart/dist/jquery.easypiechart.min .js')}}></script>
+		<script src={{asset('js/waypoints.min .js')}}></script>
+		<script src={{asset('js/jquery.easypiechart.min .js')}}></script>
+		<script src={{asset('js/classie .js')}}></script>
 
 		<!--Switcher-->
-		<script src="assets/switcher/js/switcher.js"></script>
+		<script src={{asset('assets/switcher/js/switcher .js')}}></script>
 		<!--Owl Carousel-->
-		<script src="assets/owl-carousel/owl.carousel.min.js"></script>
+		<script src={{asset('assets/owl-carousel/owl.carousel.min .js')}}></script>
 		<!--bxSlider-->
-		<script src="assets/bxslider/jquery.bxslider.js"></script>
+		<script src={{asset('assets/bxslider/jquery.bxslider .js')}}></script>
 		<!-- jQuery UI Slider -->
-		<script src="assets/slider/jquery.ui-slider.js"></script>
+		<script src={{asset('assets/slider/jquery.ui-slider .js')}}></script>
 
 		<!--Theme-->
-		<script src="js/jquery.smooth-scroll.js"></script>
-		<script src="js/wow.min.js"></script>
-		<script src="js/jquery.placeholder.min.js"></script>
-		<script src="js/theme.js"></script>
+		<script src={{asset('js/jquery.smooth-scroll .js')}}></script>
+		<script src={{asset('js/wow.min .js')}}></script>
+		<script src={{asset('js/jquery.placeholder.min .js')}}></script>
+		<script src={{asset('js/theme .js')}}></script>
 
 	</body>
 </html>
