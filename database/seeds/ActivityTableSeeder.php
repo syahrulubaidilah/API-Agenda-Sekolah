adaff55a58ec4df2941630774a19cf3984e2ade1
Syahrul Ubaidilah <syahrul923a@gmail.com>
2017-06-12 14:47:45 +0700

    data seeder calendar

--- database/seeds/ActivityTableSeeder.php
+++ database/seeds/ActivityTableSeeder.php
@@ -16,7 +16,7 @@ class ActivityTableSeeder extends Seeder
         // DB::table('activities')->truncate();
 
         // // generate dummy data with 100 records using Faker
-        factory('App\Domain\Entities\Activity', 100)->create();
+        factory('App\Domain\Entities\Activity', 10)->create();
         // $activities = [
         //     ['id' => 1, 'name' => 'a', 'description' => 'b', 'date_activity' => '12-4-2017', 'time' => '12.00', 'place' => 'aula', 'participant' => 'all class', 'user_id' => 1, 'created_at' => \Carbon\Carbon::now()],
         //     ['id' => 2, 'name' => 'a', 'description' => 'b', 'date_activity' => '12-4-2017', 'time' => '12.00', 'place' => 'aula', 'participant' => 'all class', 'user_id' => 1, 'created_at' => \Carbon\Carbon::now()],
